54eb3eddd2c977c4631f01d766fcd3292fb7484f
sakhyat <sakhyatkabir1@gmail.com>
2023-01-09 11:32:34 -0500

    project done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from "./components/Navbar";
 import {Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
@@ -10,6 +12,7 @@ const App = () => {
     return (
         <>
             <div className="min-h-screen bg-gray-50">
+                <ToastContainer/>
                 <Navbar/>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 const initialState = {
 	cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
@@ -14,10 +15,31 @@ const cartSlice = createSlice({
 			const existedItemIndex = state.cartItems.findIndex((item) => item.id === action.payload.id);
 			if (existedItemIndex >= 0) {
 				state.cartItems[existedItemIndex].cartTotalQuantity += 1;
+				toast.success("Quantity Increased", {
+					position: "bottom-left",
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: "light",
+				});
 			} else {
 				const newData = { ...action.payload, cartTotalQuantity: 1 };
 				state.cartItems.push(newData);
+				toast.success("Product Added", {
+					position: "bottom-left",
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: "light",
+				});
 			}
+
 			localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
 		},
 		removeCart(state, action) {
@@ -33,6 +55,16 @@ const cartSlice = createSlice({
 			const indexItem = state.cartItems.findIndex((item) => item.id === action.payload.id);
 			if (state.cartItems[indexItem].cartTotalQuantity > 1) {
 				state.cartItems[indexItem].cartTotalQuantity -= 1;
+				toast.warn("Quantity Decreased", {
+					position: "bottom-left",
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: "light",
+				});
 			} else if (state.cartItems[indexItem].cartTotalQuantity === 1) {
 				const updateCartItem = state.cartItems.filter((item) => item.id !== action.payload.id);
 				state.cartItems = updateCartItem;
@@ -46,7 +78,7 @@ const cartSlice = createSlice({
 				acc += itemTotal;
 				return acc;
 			}, 0);
-            state.cartTotalAmount = subTotal
+			state.cartTotalAmount = subTotal;
 		},
 	},
 });
