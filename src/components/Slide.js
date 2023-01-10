e295c51eceda1fdac5854b5301a39a6383cbb15d
sakhyat <sakhyatkabir1@gmail.com>
2023-01-10 04:53:18 -0800

    slight design fix

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -5,8 +5,8 @@ const Slide = ({item}) => {
     return (
         <div className="container mx-auto">
             <div className="header-content text-white flex flex-col items-start justify-center gap-5 font-bold">
-                <h1 className="uppercase text-6xl">{item.headline}</h1>
-                <p className="w-2/4 leading-7">{item.body}</p>
+                <h1 className="uppercase text-4xl md:text-6xl">{item.headline}</h1>
+                <p className="w-full md:w-2/4 leading-7">{item.body}</p>
                 <Link
                     className="relative uppercase border-2 hover:text-white hover:border-orange-500 py-3 px-8"
                     to={`/products/${item.category}`}>
