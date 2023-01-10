6b82299af01b5181adce124d8d75eafe2f681221
sakhyat <76532498+sakhyat@users.noreply.github.com>
2023-01-10 06:51:15 -0600

    Update Slide.js

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -5,7 +5,7 @@ const Slide = ({item}) => {
     return (
         <div className="container mx-auto">
             <div className="header-content text-white flex flex-col items-start justify-center gap-5 font-bold">
-                <h1 className="uppercase text-6xl">{item.headline}</h1>
+                <h1 className="uppercase text-4xl md:text-6xl">{item.headline}</h1>
                 <p className="w-2/4 leading-7">{item.body}</p>
                 <Link
                     className="relative uppercase border-2 hover:text-white hover:border-orange-500 py-3 px-8"
