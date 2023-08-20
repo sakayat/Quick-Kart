2331c3689607c98d54cba0dc2790f83e3e657d2d
sakhyat <sakhyatkabir1@gmail.com>
2023-08-21 01:05:49 +0600

    updated

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,14 +2,11 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { currencyFormat } from "../utilities/currencyFormate";
-import { addToCart, clearCart, decreaseCart, getSubTotal, removeCart } from "../features/cartSlice";
+import { addToCart, clearCart, decreaseCart, getSubTotal } from "../features/cartSlice";
 
 const Cart = () => {
 	const dispatch = useDispatch();
 	const { cartItems: data, cartTotalAmount: total } = useSelector((state) => state.cart);
-	const removeCartItem = (product) => {
-		dispatch(removeCart(product));
-	};
 	const decreaseCartItem = (product) => {
 		dispatch(decreaseCart(product));
 	};
@@ -51,11 +48,7 @@ const Cart = () => {
 								<img className="h-20" src={product.image} alt={product.name} />
 								<div>
 									<h4>{product.name}</h4>
-									<button
-										onClick={() => removeCartItem(product)}
-										className="uppercase text-sm text-gray-500 py-2">
-										Remove
-									</button>
+									
 								</div>
 							</div>
 							<span className="md:text-start text-end">{currencyFormat(product.price)}</span>
